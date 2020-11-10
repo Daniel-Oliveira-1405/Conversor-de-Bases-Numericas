2ee930470b059a8292b774e476d5f345524ef2b0
Daniel-Oliveira-1405 <74271848+Daniel-Oliveira-1405@users.noreply.github.com>
2020-11-10 19:58:34 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int base, n;
+    int resto, a, i;
+
+    char caracteres[36] = {"0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+    char resultado[100];
+
+    printf("Digite a base para qual ser convertida: ");
+    scanf("%d",&base);
+    printf("\nDigite o numero: ");
+    scanf("%d",&n);
+
+    if(base==8)
+        printf("%d na base 8 = %o\n",n,n);
+    else if(base==16)
+        printf("%d na base 16 = %X",n,n);
+    else{
+        a=n;
+        for(i=0; a!=0; i++){
+            resto = a % base;
+            resultado[i] = caracteres[resto];
+            a-=resto;
+            a/=2;
+        }
+        printf("%d na base %d = ",n,base);
+        for(i=strlen(resultado)-1; i>=0; i--)
+            printf("%c",resultado[i]);
+    }
+    getchar();
+}
